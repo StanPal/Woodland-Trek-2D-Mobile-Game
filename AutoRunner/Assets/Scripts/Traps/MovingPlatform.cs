784cbeb622441a575e2b153c95eb1e1fd437d0fa
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2021-11-24 20:11:32 -0800

    -Added moving sawblade -created an Imovable interface for any future objects that would move on screen -Added spinning animation to sawblade

--- AutoRunner/Assets/Scripts/Traps/MovingPlatform.cs
+++ AutoRunner/Assets/Scripts/Traps/MovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class MovingPlatform : MonoBehaviour, IMovable
 {
     public List<Transform> Points;
     public Transform platform;
@@ -16,14 +16,15 @@ public class MovingPlatform : MonoBehaviour
     {
         MoveToNextPoint();
     }
+    
 
     public void MoveToNextPoint()
     {
         platform.position = Vector2.MoveTowards(platform.position, Points[_endPoint].position, Time.deltaTime * _moveSpeed);
 
-        if(Vector2.Distance(platform.position, Points[_endPoint].position) < _distanceBetweenPoints)
+        if (Vector2.Distance(platform.position, Points[_endPoint].position) < _distanceBetweenPoints)
         {
-            if(_endPoint == Points.Count - 1)
+            if (_endPoint == Points.Count - 1)
             {
                 _endPoint = 0;
             }
@@ -32,6 +33,5 @@ public class MovingPlatform : MonoBehaviour
                 _endPoint++;
             }
         }
-
     }
 }
