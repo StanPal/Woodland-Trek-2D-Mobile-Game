d858a97830c651c104eb205d52566d88432a353c
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2022-01-07 21:41:34 -0800

    - Setup playerprefs to store collected coins locally

--- AutoRunner/Assets/Scripts/Character/Character.cs
+++ AutoRunner/Assets/Scripts/Character/Character.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerCollision _playerCollision;
+
+
     void Start()
     {
-        
+        _playerCollision = GetComponent<PlayerCollision>();
+        _playerCollision.OnCoinPickup += CoinCollected;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void CoinCollected()
+    {
+        PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + 1 );
+        Debug.Log(PlayerPrefs.GetInt("Coins"));
     }
 }
