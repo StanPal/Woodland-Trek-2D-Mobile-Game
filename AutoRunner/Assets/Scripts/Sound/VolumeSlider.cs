15c1cfa512f41634c3cd8f4b3fe7b9f14b2bd579
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2022-01-23 23:47:30 -0800

    - Edit text in thank you screen - Set static float to keep track of slider value.

--- AutoRunner/Assets/Scripts/Sound/VolumeSlider.cs
+++ AutoRunner/Assets/Scripts/Sound/VolumeSlider.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
-    [SerializeField] private Slider _slider; 
-    
+    [SerializeField] private Slider _slider;
+    private static float _sliderNum;
+
     void Start()
     {
-        SoundManager.Instance.ChangeMasterVolume(_slider.value);
+        _sliderNum = _slider.value;
+        SoundManager.Instance.ChangeMasterVolume(_sliderNum);
         _slider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeMasterVolume(val));
     }
 }
