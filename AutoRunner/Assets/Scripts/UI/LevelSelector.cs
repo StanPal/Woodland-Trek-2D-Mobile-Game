c78473d3080dc1957ac8239caf8514999e801c87
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2022-02-08 20:14:46 -0800

    - Set up scene for Level Select (IP) Creating a script that will instantiate levels and icons onto a UI grid

--- AutoRunner/Assets/Scripts/UI/LevelSelector.cs
+++ AutoRunner/Assets/Scripts/UI/LevelSelector.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject levelHolder;
+    public GameObject levelIcon;
+    public GameObject thisCanvas; 
+    [SerializeField] private int _numberOfLevels;
+    private Rect panelDimensions;
+    private Rect iconDimensions;
+    private int amountPerPage;
+    private int _currentLevelCount;
+
     void Start()
     {
-        
+        panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
+        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        int maxInARow = Mathf.FloorToInt(panelDimensions.width / iconDimensions.width);
+        int maxInACol = Mathf.FloorToInt(panelDimensions.height / iconDimensions.height);
+        amountPerPage = maxInARow * maxInACol;
+        int totalPages = Mathf.CeilToInt((float)_numberOfLevels / amountPerPage);
+        LoadPanels(totalPages); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadPanels(int nubmerofPanels)
     {
+        GameObject panelClone = Instantiate(levelHolder) as GameObject; 
+
+        for(int i = 1; i <= _numberOfLevels; i++)
+        {
+            GameObject panel = Instantiate(panelClone) as GameObject;
+            panel.transform.SetParent(thisCanvas.transform, false);
+            panel.transform.SetParent(levelHolder.transform);
+            panel.name = "Page-" + i;
+            panel.GetComponent<RectTransform>().localPosition = new Vector2(panelDimensions.width * (i - 1), 0);
+            SetUpGrid(panel);
+            int numberOfIcons = i == amountPerPage ? _numberOfLevels - _currentLevelCount : amountPerPage; 
+            LoadIcons(numberOfIcons, panel);
+        }
+        Destroy(panelClone);
+    }
+
+    void LoadIcons(int numberOfIcons, GameObject parentObject)
+    {
+        for(int i = 1; i <= numberOfIcons; i++)
+        {
+            _currentLevelCount++;
+            GameObject icon = Instantiate(levelIcon) as GameObject;
+            icon.transform.SetParent(thisCanvas.transform, false);
+            icon.transform.SetParent(parentObject.transform);
+            icon.name = "Level " + i;
+        }
+    }
+    
+    private void SetUpGrid(GameObject panel)
+    {
+        GridLayoutGroup grid = panel.AddComponent<GridLayoutGroup>();
+        grid.cellSize = new Vector2(iconDimensions.width, iconDimensions.height);
+        grid.childAlignment = TextAnchor.MiddleCenter;
         
     }
+    
 }
