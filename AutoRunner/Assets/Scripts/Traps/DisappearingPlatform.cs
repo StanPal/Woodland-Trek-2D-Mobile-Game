94228ea58c10b2d4e4696b1cfc5d2def635e0fbb
Stan Palisoc <46430858+StanPal@users.noreply.github.com>
2022-02-28 20:12:24 -0800

    - Created Dissapearing Platforms that fade in transparency over time. - Created Level 18 which introduces the new trap. - Created Script that controls the rate of which they dissapear and when they can reappear. - Removed previous platform prefab.

--- AutoRunner/Assets/Scripts/Traps/DisappearingPlatform.cs
+++ AutoRunner/Assets/Scripts/Traps/DisappearingPlatform.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class DisappearingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _timeToTogglePlatform;
+    [SerializeField] private float _fadeSpeed;
+    [SerializeField] private float _returnDelay;
+    [SerializeField] private float _alphaFadeValue;
+    private float _currentTime = 0;
+    private bool _isEnabled = true;
+    private bool _isFading; 
+
+    private void Start()
     {
-        
+        _isEnabled = true; 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _currentTime += Time.deltaTime;
+        if(_currentTime >= _timeToTogglePlatform && !_isFading)
+        {
+            _currentTime = 0;
+            TogglePlatform();
+        }
     }
+
+    private void TogglePlatform()
+    {
+        _isEnabled = !_isEnabled;
+        foreach(Transform child in gameObject.transform)
+        {
+            StartCoroutine(PlatformFade(child.gameObject, _isEnabled));
+            //child.gameObject.SetActive(_isEnabled);
+        }
+    }
+
+    private IEnumerator PlatformFade(GameObject platform, bool enabled)
+    {
+        float alphaValue = 1.0f;
+        SpriteRenderer platformSprite = platform.GetComponent<SpriteRenderer>();
+        if (enabled)
+        {
+            yield return new WaitForSeconds(_returnDelay);
+            platform.SetActive(enabled);
+            platformSprite.material.color = new Color(platformSprite.material.color.r,
+                  platformSprite.material.color.b, platformSprite.material.color.g, alphaValue);
+        }
+        else
+        {
+            _isFading = true;
+            while (platformSprite.material.color.a > 0)
+            {
+                platformSprite.material.color = new Color(platformSprite.material.color.r,
+                    platformSprite.material.color.b, platformSprite.material.color.g, alphaValue);
+                yield return new WaitForSeconds(_fadeSpeed);
+                alphaValue -= _alphaFadeValue;
+            }
+            platform.SetActive(enabled);
+            _isFading = false;
+        }
+    }
+
 }
